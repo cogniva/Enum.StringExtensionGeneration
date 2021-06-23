9dc203e7b0c286b913211c705101080aff89eee2
Christine Salter <c.salter@cogniva.ca>
2021-06-23 14:29:27 -0400

    Initial implementation

---
+++ EnumStringExtensionGenerator/EnumStringExtensionGenerator.Test/SampleSourceCodeGenerator.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EnumStringExtensionGenerator.Test
+{
+    /// <summary>
+    /// A collection of extension methods to somewhat simplify the creation of source code for use in testing.
+    /// </summary>
+    /// <remarks>
+    /// These are intended to be very simple, fairly brute-force tools, aiming primarily at making the scenario clear
+    /// rather than being perfectly-designed source generation tools themselves.
+    /// </remarks>
+    public static class SampleSourceCodeGenerator
+    {
+        public const string ExtensionNamespace = "EnumStringExtensionGenerator";
+
+        #region Generation of enum classes
+
+        public static string MakeEnumWithValues(this string enumName, params string[] valueNames)
+        {
+            return $@"public enum {enumName}
+{{
+{ string.Join(",\n", valueNames).Indent()}
+}}";
+        }
+
+        public static string WithLocalisation(this string code, string localisationNamespace, string localisationClassName, string defaultName = null)
+        {
+            string defaultPropertyNameParam = string.IsNullOrWhiteSpace(defaultName)
+                ? ""
+                : $@", DefaultPropertyName=""{defaultName}""";
+            return
+                $@"[GenerateLocalisation(typeof({localisationClassName.WithNamespacePrefix(localisationNamespace)}){defaultPropertyNameParam})]
+{code}";
+
+        }
+
+        #endregion
+
+        #region Generation of a mock that works as a resource class
+
+        public static string MakeLocalisedStrings(this string className, params (string PropertyName, string ReturnValue)[] properties)
+        {
+            static string MakePropertyLine((string PropertyName, string ReturnValue) property)
+            {
+                return $@"public static string {property.PropertyName} => ""{property.ReturnValue}"";";
+            }
+
+            return $@"public class {className}
+{{
+{string.Join("\n", properties.Select(MakePropertyLine)).Indent()}
+}}
+";
+        }
+
+        #endregion
+
+        #region Generation of pairs of property name & return value, for use in creating a mock of a resource class
+
+        public static Func<string, (string Name, string ReturnValue)> PairGenerator(this string enumName)
+        {
+            return valueName => MakePair($"{enumName}{{0}}Description", valueName);
+        }
+
+        private static (string Name, string ReturnValue) MakePair(string enumFormat, string valueName)
+        {
+            return (string.Format(enumFormat, valueName), valueName);
+        }
+
+        #endregion
+
+        #region Creating a test class to actually use the generated code
+
+        public static string CallEnumValues(this string enumName, params string[] enumValues)
+        {
+            static string LogSingleValue(string enumName, string enumValue)
+            {
+                return @$"    System.Console.WriteLine(""Value of {enumName}.{enumValue} is {{0}}"", {enumName}.{enumValue}.GetDescription());";
+            }
+
+            return string.Join("\n",
+                enumValues.Select(enumValue => LogSingleValue(enumName, enumValue)))
+                .Indent();
+        }
+
+        public static string CreateCallingFile(string enumNamespace, string enumName, params string[] enumValues)
+        {
+            var callingFile = $@"public class TestingClass 
+{{
+    public static void Main(string[] args)
+    {{
+{CallEnumValues(enumName, enumValues)}
+    }}
+}}"
+                .InNamespace("Testing")
+                .Using("System");
+            if (!string.IsNullOrWhiteSpace(enumNamespace))
+                callingFile = callingFile.Using(enumNamespace);
+            return callingFile;
+        }
+
+        #endregion
+
+        public static string UsingExtensionNamespace(this string file)
+        {
+            return file.Using(ExtensionNamespace);
+        }
+
+        public static string Using(this string file, params string[] namespaces)
+        {
+            return $@"{string.Join("\n", namespaces.Select(namespaceName => $"using {namespaceName};"))}
+{file}";
+        }
+
+        public static string WithNamespacePrefix(this string typeName, string namespaceName)
+        {
+            return string.IsNullOrWhiteSpace(namespaceName) ? typeName : $"{namespaceName}.{typeName}";
+        }
+    }
+}
---
+++ EnumStringExtensionGenerator/EnumStringExtensionGenerator/GenerationError.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace EnumStringExtensionGenerator
+{
+    public enum GenerationErrorCode
+    {
+        UnspecifiedError,
+        MissingRequiredDefaultPropertyName,
+        DefaultPropertyNameInvalid,
+    }
+
+    public class GenerationError
+    {
+        public static class PropertyNames
+        {
+            public const string OriginalDefaultPropertyName = nameof(OriginalDefaultPropertyName);
+            public const string ExceptionMessage = nameof(ExceptionMessage);
+        }
+
+        public string EnumTypeName { get; }
+        public GenerationErrorCode ErrorCode { get; }
+        public Dictionary<string, object> ExtraData { get; }
+
+        public GenerationError(string enumTypeName, GenerationErrorCode errorCode)
+            : this(enumTypeName, errorCode, null)
+        {
+        }
+        public GenerationError(string enumTypeName, GenerationErrorCode errorCode, Dictionary<string, object> extraData)
+        {
+            EnumTypeName = enumTypeName;
+            ErrorCode = errorCode;
+
+            ExtraData = new Dictionary<string, object>(extraData ?? new Dictionary<string, object>());
+        }
+
+        public void AddData(string key, object value)
+        {
+            ExtraData[key] = value;
+        }
+    }
+
+    public static class ErrorCodeExtensions
+    {
+        public static string Id(this GenerationErrorCode code)
+        {
+            return $"ESG{(int)code:0000}";
+        }
+    }
+}
